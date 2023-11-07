b88144abf3e7705ee2d3a995125d0caab21fbbb2
Juraj Antas <juraj.antas@gmail.com>
2023-11-07 22:34:29 +0100

    1.0.0

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftLog",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "SwiftLog",
+            targets: ["SwiftLog"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SwiftLog",
+            dependencies: [
+            ],
+            path: "Sources",
+            exclude: [
+                
+            ],
+            resources: [
+            ])
+    ]
+)
